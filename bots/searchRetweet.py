ed2c2158e9f6320caa04181aef0966a892a84954
apcabotaje <aymancabotaje.dev@gmail.com>
2020-11-24 15:17:02 +0800

    increase sleep time

--- bots/searchRetweet.py
+++ bots/searchRetweet.py
@@ -6,19 +6,16 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-tweets = tweepy.Cursor(api.search, q=('#100DaysOfCode OR #Python -filter:retweets'), lang='en').items(5)
+tweets = tweepy.Cursor(api.search, q=('#100DaysOfCode OR #Python -filter:retweets'), lang='en').items()
 
-def searchRetweet():
-    for tweet in tweets:
-        print("Searching Tweets...")
-        try:
-            tweet.retweet()
-            print("Retweet Done!")
-            time.sleep(10)
-        except tweepy.TweepError as e:
-            print(e.reason)
-            time.sleep(10)
 
-while True:
-    searchRetweet()
-    time.sleep(30)
+for tweet in tweets:
+    print("Searching Tweets...")
+    try:
+        tweet.retweet()
+        print("Retweet Done!")
+        time.sleep(300)
+    except tweepy.TweepError as e:
+        print(e.reason)
+        time.sleep(300)
+
